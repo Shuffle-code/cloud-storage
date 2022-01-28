20b9c599dc5385faf3c83620c95e3d8237708562
Evgeniy <malugn-evg-nsk@yandex.ru>
2022-01-28 11:22:06 +0700

    Add comment

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -9,9 +9,11 @@ public class Server {
 
         ServerSocket server = new ServerSocket(8189);
         while (true) {
-            Socket socket = server.accept();
+            Socket socket = server.accept(); // подключили нового клиента. Объявляется Soket на стороне клиента, а сервер воссоздаёт его, получая сигнал на подключение.
+            // Тут подключается метод accept(). Искомый ждёт пока кто-либо не захочет подсоединится к нему, и когда это происходит возвращает объект типа Socket,
+            // то есть воссозданный клиентский сокет. И вот когда сокет клиента создан на стороне сервера, можно начинать двухстороннее общение.
             System.out.println("New client connected...");
-            new Thread(new Handler(socket)).start();
+            new Thread(new Handler(socket)).start(); // запустили в отдельном потоке, через класс Handler
         }
 
     }
